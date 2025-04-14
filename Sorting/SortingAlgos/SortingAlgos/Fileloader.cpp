dbc2ecdd362b781f99341b50663759c5f8f904ac
Barto9 <bartekgawryszuk@gmail.com>
2025-04-14 18:13:29 +0200

    fileloader work start

---
+++ Sorting/SortingAlgos/SortingAlgos/Fileloader.cpp
@@ -0,0 +1,1 @@
+#include "Fileloader.h"
---
+++ Sorting/SortingAlgos/SortingAlgos/Fileloader.h
@@ -0,0 +1,9 @@
+#pragma once
+class Fileloader
+{/* TODO:
+	-Load file to array -> i know this shit
+	-Save results to file -> check how Chat does it
+	I think that's it?
+ */
+};
+
