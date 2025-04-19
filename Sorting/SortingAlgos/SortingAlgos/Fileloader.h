4b37ce9ef4481250e22439a38eebef285b52f581
Barto9 <bartekgawryszuk@gmail.com>
2025-04-19 19:00:04 +0200

    file shenanigans (save and load)

--- Sorting/SortingAlgos/SortingAlgos/Fileloader.h
+++ Sorting/SortingAlgos/SortingAlgos/Fileloader.h
@@ -1,9 +1,20 @@
 #pragma once
+#include <fstream>
+#include <iostream>
 class Fileloader
 {/* TODO:
 	-Load file to array -> i know this shit
 	-Save results to file -> check how Chat does it
 	I think that's it?
  */
+public:
+	int* array = nullptr;
+	int size = 0;
+	int LoadFile(const std::string filename);
+	int SaveToFile(const std::string& filename) const;
+	void DisplayArray() const;
+	
+
+	
 };
 
--- Sorting/SortingAlgos/SortingAlgos/SortingAlgos.cpp
+++ Sorting/SortingAlgos/SortingAlgos/SortingAlgos.cpp
@@ -1,42 +1,13 @@
 ﻿#include <iostream>
 #include <thread>
 #include "Timer.h"
+#include "Fileloader.h"
 
 int main() {
     Timer timer;
-    char command;
-
-    while (true) {
-        std::cout << "Press 's' to start, 'e' to stop, 'r' to reset, 'q' to quit: ";
-        std::cin >> command;
-
-        switch (command) {
-        case 's':
-            if (timer.start() == 0)
-                std::cout << "Timer started.\n";
-            else
-                std::cout << "Timer is already running!\n";
-            break;
-
-        case 'e':
-            if (timer.stop() == 0)
-                std::cout << "Timer stopped. Elapsed time: " << timer.result() << " ms\n";
-            else
-                std::cout << "Timer is not running!\n";
-            break;
-
-        case 'r':
-            timer.reset();
-            std::cout << "Timer reset.\n";
-            break;
-
-        case 'q':
-            std::cout << "Exiting...\n";
-            return 0;
-
-        default:
-            std::cout << "Invalid input. Try again.\n";
-            break;
-        }
-    }
+    Fileloader fileloader;
+    fileloader.LoadFile("test.txt");
+    std::cout << "File loaded successfully." << std::endl;
+    fileloader.DisplayArray();
+    return 0;
 }
